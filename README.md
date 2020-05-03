5d33decfc61db679f88b6a6541b83019cce97f53
laurenlittle <littlelauren016@gmail.com>
2020-05-03 11:38:17 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Take-cmd
+
+## Table of contents
+* [Introduction](#introduction)
+* [Technologies](#technologies)
+* [Installation](#installation)
+
+
+## Introduction
+Take control of your career by creating your own career management document. Simple and easy to use, this allows you keep a running collection of your daily/weekly accomplishments, work completed, and career milestones all in one place.
+
+By having this comprehensive documentation of your work, updating resumes and portfolios becomes painless.
+
+Inspired by the article: https://alistapart.com/article/the-career-management-document/
+
+
+## Technologies
+This project has been created with:
+* React
+* Firebase
+
+
+## Installation
+To run this project, install it locally using npm:
+```
+$ npm install
+$ npm run dev
+```
---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+module.exports = (env) => {
+  console.log(env)
+  return {
+    entry: './src/index.js',
+    mode: env.mode,
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        },
+        {
+          test: /\.html$/,
+          use: ['html-loader']
+        },
+        {
+          test: /\.module\.s(a|c)ss$/, // SCSS Modules
+          loader: [
+            isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                sourceMap: isDevelopment
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isDevelopment
+              }
+            }
+          ]
+        },
+        {
+          test: /\.s(a|c)ss$/, // SCSS
+          exclude: /\.module.(s(a|c)ss)$/,
+          loader: [
+            isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isDevelopment
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+            'url-loader',
+          ],
+        },
+      ]
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx', '.scss']
+    },
+    output: {
+      path: __dirname + '/dist',
+      publicPath: '/',
+      filename: 'bundle.js'
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html'
+      }),
+      new MiniCssExtractPlugin({
+        filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+        chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+      }),
+      new webpack.ProgressPlugin()
+    ]
+  };
+};
